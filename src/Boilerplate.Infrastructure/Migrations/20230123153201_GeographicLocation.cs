0019081eaf0f4b605f2e1ee7026e13f470894995
Raúl <raulidavid@hotmail.com>
2023-01-23 11:06:58 -0500

    GeographicLocation

---
+++ src/Boilerplate.Infrastructure/Migrations/20230123153201_GeographicLocation.cs
@@ -0,0 +1,22 @@
+﻿using Boilerplate.Infrastructure.Configuration;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Boilerplate.Infrastructure.Migrations;
+
+/// <inheritdoc />
+public partial class GeographicLocation : Migration
+{
+    /// <inheritdoc />
+    protected override void Up(MigrationBuilder migrationBuilder)
+    {
+        migrationBuilder.Sql(Utils.GetRawSql("20230123153201_GeographicLocationUp.sql"));
+    }
+
+    /// <inheritdoc />
+    protected override void Down(MigrationBuilder migrationBuilder)
+    {
+        migrationBuilder.Sql(Utils.GetRawSql("20230123153201_GeographicLocationDown.sql"));
+    }
+}
---
+++ src/Boilerplate.Infrastructure/Scripts/20230123153201_GeographicLocationUp.sql
@@ -0,0 +1,51 @@
+-- Table: web."GeographicLocation"
+
+-- DROP TABLE IF EXISTS web."GeographicLocation";
+
+CREATE TABLE IF NOT EXISTS web."GeographicLocation"
+(
+    "Id" serial NOT NULL,
+    "Code" character varying(50) COLLATE pg_catalog."default" NOT NULL DEFAULT 0,
+    "Name" character varying(150) COLLATE pg_catalog."default" NOT NULL DEFAULT 0,
+    "ParentId" integer DEFAULT 0,
+    "Parroquia" smallint NOT NULL DEFAULT 0,
+    CONSTRAINT GeographicLocation_Id_pkey PRIMARY KEY ("Id"),
+    CONSTRAINT GeographicLocation_ParentId_fkey FOREIGN KEY ("ParentId")
+        REFERENCES web."GeographicLocation" ("Id") MATCH SIMPLE
+        ON UPDATE NO ACTION
+        ON DELETE NO ACTION
+)
+WITH (
+    OIDS = FALSE
+)
+TABLESPACE pg_default;
+
+ALTER TABLE IF EXISTS web."GeographicLocation"
+    OWNER to postgres;
+
+COMMENT ON TABLE web."GeographicLocation"
+    IS 'TABLA CON LA DISTRIBUCION GEOGRÁFICA DEL ECUADOR PROVINCIAS, CANTONES, PARROQUIAS';
+-- Index: GeographicLocation_ParentId_idx
+
+-- DROP INDEX IF EXISTS web.GeographicLocation_ParentId_idx;
+
+CREATE INDEX IF NOT EXISTS GeographicLocation_ParentId_idx
+    ON web."GeographicLocation" USING btree
+    ("ParentId" ASC NULLS LAST)
+    TABLESPACE pg_default;
+	
+insert into web."GeographicLocation"
+(
+	"Id",
+	"Code",
+	"Name",
+	"ParentId",
+	"Parroquia"
+)
+select 
+    "id",
+	"codigo",
+	"nombre",
+	"idpadre",
+	"parroquia"
+    FROM  public.ubicaciongeografica;
