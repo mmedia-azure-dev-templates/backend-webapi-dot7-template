a1ad8d5878de69b5ecb77443584367debfb6b4ee
Raul <raulidavid@hotmail.com>
2023-02-06 16:53:45 -0500

    api success

---
+++ src/Boilerplate.Api/Controllers/DefaultController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Boilerplate.Api.Controllers;
+
+[Route("/")]
+[AllowAnonymous]
+public class DefaultController : ControllerBase
+{
+    [HttpGet]
+    public IActionResult Index()
+    {
+        return Ok("API SUCCESSFULLY STARTED");
+    }
+}
