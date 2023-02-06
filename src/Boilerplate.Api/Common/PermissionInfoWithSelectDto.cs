8523f3bbd562301ec920b33d0154d7b002aeaf02
Raul <raulidavid@hotmail.com>
2023-02-06 08:27:49 -0500

    ADD ADMINISTRATIVE CONTROLLERS

---
+++ src/Boilerplate.Api/Common/PermissionInfoWithSelectDto.cs
@@ -0,0 +1,13 @@
+﻿// Copyright (c) 2021 Jon P Smith, GitHub: JonPSmith, web: http://www.thereformedprogrammer.net/
+// Licensed under MIT license. See License.txt in the project root for license information.
+
+namespace Boilerplate.Api.Common
+{
+    public class PermissionInfoWithSelectDto
+    {
+        public string GroupName { get; set; }
+        public string Description { get; set; }
+        public string PermissionName { get; set; }
+        public bool Selected { get; set; }
+    }
+}
---
+++ src/Boilerplate.Api/Controllers/TenantAdminController.cs
@@ -0,0 +1,122 @@
+﻿using AuthPermissions.AdminCode;
+using AuthPermissions.AspNetCore;
+using AuthPermissions.BaseCode.CommonCode;
+using AuthPermissions.SupportCode.AddUsersServices;
+using Boilerplate.Api.Common;
+using Boilerplate.Domain.PermissionsCode;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Boilerplate.Api.Controllers;
+
+[ApiController]
+[Route("api/[controller]")]
+public class TenantAdminController : Controller
+{
+    private readonly IAuthUsersAdminService _authUsersAdmin;
+
+    public TenantAdminController(IAuthUsersAdminService authUsersAdmin)
+    {
+        _authUsersAdmin = authUsersAdmin;
+    }
+
+    [HasPermission(Example3Permissions.UserRead)]
+    [HttpGet]
+    public async Task<IActionResult> Index(string message)
+    {
+        var dataKey = User.GetAuthDataKeyFromUser();
+        var userQuery = _authUsersAdmin.QueryAuthUsers(dataKey);
+        var usersToShow = await AuthUserDisplay.TurnIntoDisplayFormat(userQuery.OrderBy(x => x.Email)).ToListAsync();
+
+        ViewBag.Message = message;
+
+        return Ok();
+    }
+    
+    [HasPermission(Example3Permissions.UserRolesChange)]
+    [HttpGet]
+    [Route("edit-roles")]
+    public async Task<ActionResult> EditRoles(string userId)
+    {
+        var status = await SetupManualUserChange.PrepareForUpdateAsync(userId, _authUsersAdmin);
+        if (status.HasErrors)
+            return Ok(status.GetAllErrors());
+            //new { errorMessage = status.GetAllErrors() });
+
+        return View(status.Result);
+    }
+    
+    [HttpPost]
+    [ValidateAntiForgeryToken]
+    [HasPermission(Example3Permissions.UserRolesChange)]
+    [Route("edit-roles")]
+    public async Task<ActionResult> EditRoles(SetupManualUserChange change)
+    {
+        var status = await _authUsersAdmin.UpdateUserAsync(change.UserId,
+            roleNames: change.RoleNames);
+
+        if (status.HasErrors)
+            return Ok(status.GetAllErrors());
+                //new { errorMessage = status.GetAllErrors() });
+
+        return RedirectToAction(nameof(Index), new { message = status.Message });
+    }
+
+
+    [HasPermission(Example3Permissions.InviteUsers)]
+    [HttpGet]
+    [Route("invite-user")]
+    public async Task<ActionResult> InviteUser([FromServices] IInviteNewUserService inviteService)
+    {
+        var setupInvite = new InviteUserSetup
+        {
+            AllRoleNames = await _authUsersAdmin.GetRoleNamesForUsersAsync(User.GetUserIdFromUser()),
+            ExpirationTimesDropdown = inviteService.ListOfExpirationTimes()
+        };
+
+        return View(setupInvite);
+    }
+
+    [HasPermission(Example3Permissions.InviteUsers)]
+    [HttpPost]
+    [ValidateAntiForgeryToken]
+    [Route("invite-user")]
+    public async Task<ActionResult> InviteUser([FromServices] IInviteNewUserService inviteUserServiceService, InviteUserSetup data)
+    {
+        var addUserData = new AddNewUserDto
+        {
+            Email = data.Email,
+            Roles = data.RoleNames,
+            TimeInviteExpires = data.InviteExpiration
+        };
+        var status = await inviteUserServiceService.CreateInviteUserToJoinAsync(addUserData, User.GetUserIdFromUser());
+        if (status.HasErrors)
+            return Ok(status.GetAllErrors());
+            //return RedirectToAction(nameof(ErrorDisplay),
+                //new { errorMessage = status.GetAllErrors() });
+        return Ok();
+        //var inviteUrl = AbsoluteAction(Url, nameof(HomeController.AcceptInvite), "Home", new { verify = status.Result });
+
+        //return View("InviteUserUrl", new InviteUserResult(status.Message, inviteUrl));
+    }
+    /*
+    public ActionResult ErrorDisplay(string errorMessage)
+    {
+        return View((object)errorMessage);
+    }
+
+    //-------------------------------------------------------
+
+    //Thanks to https://stackoverflow.com/questions/30755827/getting-absolute-urls-using-asp-net-core
+    public string AbsoluteAction(IUrlHelper url,
+        string actionName,
+        string controllerName,
+        object routeValues = null)
+    {
+        string scheme = HttpContext.Request.Scheme;
+        return url.Action(actionName, controllerName, routeValues, scheme);
+    }
+    */
+}
