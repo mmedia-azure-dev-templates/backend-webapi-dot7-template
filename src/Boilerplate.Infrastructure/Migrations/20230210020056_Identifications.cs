846434809609ba8c41e9e900f5c1c5e38314e370
Raúl Flores <raulidavid@hotmail.com>
2023-02-22 06:17:00 -0500

    Change guid

--- src/Boilerplate.Infrastructure/Migrations/20230210020056_Identifications.cs
+++ src/Boilerplate.Infrastructure/Migrations/20230210020056_Identifications.cs
@@ -18,8 +18,8 @@ public partial class Identifications : Migration
         name: "UserInformations",
         columns: table => new
         {
-            Id = table.Column<string>(type: "nvarchar(450)", nullable: false),
-            UserId = table.Column<string>(type: "nvarchar(450)", nullable: false),
+            Id = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+            UserId = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
             TypeDocument = table.Column<string>(type: "nvarchar(50)", nullable: false),
             Nacionality = table.Column<string>(type: "nvarchar(50)", nullable: false),
             Ndocument = table.Column<string>(type: "nvarchar(30)", nullable: false),
