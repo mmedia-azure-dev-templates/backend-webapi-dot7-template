3e6a9bd8179e8ef030cb22152b8a67eaa8151b9c
Raúl <raulidavid@hotmail.com>
2023-02-25 20:13:07 -0500

    Enums

---
+++ src/Boilerplate.Domain/Entities/Enums/DocumentType.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Boilerplate.Domain.Entities.Enums;
+public enum DocumentType
+{
+    [Display(Name = "Cedula")]
+    Dni,
+    [Display(Name = "Pasaporte")]
+    Passport
+}
