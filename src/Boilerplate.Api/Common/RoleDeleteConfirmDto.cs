943908478d2c5b1c4f1649a5162423b3f6307ef9
Raul <raulidavid@hotmail.com>
2023-03-29 21:47:23 -0500

    Fixed Namespace

--- src/Boilerplate.Api/Common/RoleDeleteConfirmDto.cs
+++ src/Boilerplate.Api/Common/RoleDeleteConfirmDto.cs
@@ -7,29 +7,28 @@ using System.Threading.Tasks;
 using AuthPermissions.AdminCode;
 using Microsoft.EntityFrameworkCore;
 
-namespace Boilerplate.Api.Common
+namespace Boilerplate.Api.Common;
+
+/// <summary>
+/// This is the standard delete confirm where you need to display what uses are using a Role
+/// </summary>
+public class RoleDeleteConfirmDto
 {
-    /// <summary>
-    /// This is the standard delete confirm where you need to display what uses are using a Role
-    /// </summary>
-    public class RoleDeleteConfirmDto
+    public string RoleName { get; set; }
+    public string ConfirmDelete { get; set; }
+    public List<EmailAndUserNameDto> AuthUsers { get; set; }
+
+    public static async Task<RoleDeleteConfirmDto> FormRoleDeleteConfirmDtoAsync(string roleName, IAuthRolesAdminService rolesAdminService)
     {
-        public string RoleName { get; set; }
-        public string ConfirmDelete { get; set; }
-        public List<EmailAndUserNameDto> AuthUsers { get; set; }
-
-        public static async Task<RoleDeleteConfirmDto> FormRoleDeleteConfirmDtoAsync(string roleName, IAuthRolesAdminService rolesAdminService)
+        var result = new RoleDeleteConfirmDto
         {
-            var result = new RoleDeleteConfirmDto
-            {
-                RoleName = roleName,
-                AuthUsers = await rolesAdminService.QueryUsersUsingThisRole(roleName)
-                    .Select(x => new EmailAndUserNameDto(x.Email, x.UserName))
-                    .ToListAsync()
-            };
+            RoleName = roleName,
+            AuthUsers = await rolesAdminService.QueryUsersUsingThisRole(roleName)
+                .Select(x => new EmailAndUserNameDto(x.Email, x.UserName))
+                .ToListAsync()
+        };
 
-            return result;
-        }
-
+        return result;
     }
+
 }
