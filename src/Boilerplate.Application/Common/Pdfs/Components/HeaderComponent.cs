072abddd6af6fa9f1e4bb1e431ddc860a77975ce
Raul David Flores Serrano <raulidavid@hotmail.com>
2023-04-10 20:51:51 -0500

    PDF

--- src/Boilerplate.Application/Common/Pdfs/Components/HeaderComponent.cs
+++ src/Boilerplate.Application/Common/Pdfs/Components/HeaderComponent.cs
@@ -24,51 +24,54 @@ public class HeaderComponent : IComponent
     public void Compose(IContainer container)
     {
         var titleStyle = TextStyle.Default.FontSize(15).SemiBold().FontColor(Colors.Red.Medium);
-        container.Row(row =>
+        container.Column(column =>
         {
-            row.RelativeItem().Column(column =>
+            column.Item().Row(row =>
+            {                
+                row.ConstantItem(300).AlignBottom().Background("#BBB").Column(column =>
+                {
+                    column.Item().AlignMiddle().Text(text =>
+                    {
+                        text.Span($"ORDEN DE COMPRA No. ").Bold().FontSize(16);
+                        text.Span($"{_orderByIdResponse.Order.OrderNumber}").Bold().Style(titleStyle);
+                        text.EmptyLine();
+                        text.Span("Fecha: ").SemiBold();
+                        text.Span($"{_orderByIdResponse.Order.DateCreated:F}").Light();
+                    });
+                });
+
+                row.RelativeItem(2).AlignMiddle().Image(_logo, ImageScaling.FitArea);
+            });
+
+            column.Item().Text(text =>
             {
-                column.Item().Text(text =>
-                {
-                    text.Span($"ORDEN DE COMPRA No. ").Bold().FontSize(16);
-                    text.Span($"{_orderByIdResponse.Order.OrderNumber}").Bold().Style(titleStyle);
-                    text.EmptyLine();
-                    text.Span("Fecha: ").SemiBold();
-                    text.Span($"{_orderByIdResponse.Order.DateCreated:F}").Light();
-                });
 
-                row.ConstantItem(180).Image(_logo, ImageScaling.FitArea);
+                text.Span("Nombres y Apellidos: ").SemiBold();
+                text.Span($"{_orderByIdResponse.Customer.FirstName} {_orderByIdResponse.Customer.LastName}").Light();
+                text.EmptyLine();
+                text.Span($"No. Cédula: ").SemiBold();
+                text.Span($"{_orderByIdResponse.Customer.Ndocument}").Light();
+                text.EmptyLine();
+                text.Span("Dirección domiciliar: ").SemiBold();
+                text.Span($"{_orderByIdResponse.Customer.PrimaryStreet} {_orderByIdResponse.Customer.SecondaryStreet} {_orderByIdResponse.Customer.Numeration}").Light();
+                text.EmptyLine();
+                text.Span("Referencia del domicilio: ").SemiBold();
+                text.Span($"{_orderByIdResponse.Customer.Reference}").Light();
+                text.EmptyLine();
+                text.Span("Teléfonos: ").SemiBold();
+                text.Span($"{_orderByIdResponse.Customer.Mobile}").Light();
+                text.EmptyLine();
+                text.Span("Correo electrónico: ").SemiBold();
+                text.Span($"{_orderByIdResponse.Customer.Email}").Light();
+                text.EmptyLine();
+                text.Span("Provincia: ").SemiBold();
+                text.Span($"{_orderByIdResponse.Customer.Provincia} ").Light();
+                text.Span("Canton: ").SemiBold();
+                text.Span($"{_orderByIdResponse.Customer.Canton} ").Light();
+                text.Span("Parroquia: ").SemiBold();
+                text.Span($"{_orderByIdResponse.Customer.Parroquia} ").Light();
+            });
 
-                column.Item().Text(text =>
-                {
-                    text.EmptyLine();
-                    text.EmptyLine();
-                    text.Span("Nombres y Apellidos: ").SemiBold();
-                    text.Span($"{_orderByIdResponse.Customer.FirstName} {_orderByIdResponse.Customer.LastName}").Light();
-                    text.EmptyLine();
-                    text.Span($"No. Cédula: ").SemiBold();
-                    text.Span($"{_orderByIdResponse.Customer.Ndocument}").Light();
-                    text.EmptyLine();
-                    text.Span("Dirección domiciliar: ").SemiBold();
-                    text.Span($"{_orderByIdResponse.Customer.PrimaryStreet} {_orderByIdResponse.Customer.SecondaryStreet} {_orderByIdResponse.Customer.Numeration}").Light();
-                    text.EmptyLine();
-                    text.Span("Referencia del domicilio: ").SemiBold();
-                    text.Span($"{_orderByIdResponse.Customer.Reference}").Light();
-                    text.EmptyLine();
-                    text.Span("Teléfonos: ").SemiBold();
-                    text.Span($"{_orderByIdResponse.Customer.Mobile}").Light();
-                    text.EmptyLine();
-                    text.Span("Correo electrónico: ").SemiBold();
-                    text.Span($"{_orderByIdResponse.Customer.Email}").Light();
-                    text.EmptyLine();
-                    text.Span("Provincia: ").SemiBold();
-                    text.Span($"{_orderByIdResponse.Customer.Provincia} ").Light();
-                    text.Span("Canton: ").SemiBold();
-                    text.Span($"{_orderByIdResponse.Customer.Canton} ").Light();
-                    text.Span("Parroquia: ").SemiBold();
-                    text.Span($"{_orderByIdResponse.Customer.Parroquia} ").Light();
-                });
-            }); 
         });
     }
 }
