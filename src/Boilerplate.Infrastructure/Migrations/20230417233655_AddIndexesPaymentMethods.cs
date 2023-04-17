24148343fcf5ff104aa2f6fa11f93bae26709bcd
Raul David Flores Serrano <raulidavid@hotmail.com>
2023-04-17 18:48:15 -0500

    Add Indexes

---
+++ src/Boilerplate.Infrastructure/Migrations/20230417233655_AddIndexesPaymentMethods.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Boilerplate.Infrastructure.Migrations;
+
+/// <inheritdoc />
+public partial class AddIndexesPaymentMethods : Migration
+{
+    /// <inheritdoc />
+    protected override void Up(MigrationBuilder migrationBuilder)
+    {
+        migrationBuilder.Sql(@"CREATE UNIQUE INDEX [DataKeyPaymentMethodsTypeIndex]
+        ON web.PaymentMethods (DataKey, PaymentMethodsType)
+        WHERE PaymentMethodsType IS NOT NULL ;");
+    }
+
+    /// <inheritdoc />
+    protected override void Down(MigrationBuilder migrationBuilder)
+    {
+        migrationBuilder.DropIndex(
+            name: "DataKeyPaymentMethodsTypeIndex",
+            schema: "web",
+            table: "PaymentMethods"
+        );
+    }
+}
